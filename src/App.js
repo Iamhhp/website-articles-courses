51cdcfa1be42ffb6a275d215e90541da01589c7d
Iamhhp <Iamhhp2020@gmail.com>
2023-12-28 20:38:27 +0330

    create file routes

--- src/App.js
+++ src/App.js
@@ -1,30 +1,8 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import MainRoute from './components/MainRoute/MainRoute';
-import Home from './components/pages/Home/Home';
-import Article from './components/pages/Article/Article';
-import ArticleEditCreate from './components/pages/ArticleEditCreate/ArticleEditCreate';
-import Articles from './components/pages/Articles/Articles';
-import Course from './components/pages/Course/Course';
-import Courses from './components/pages/Courses/Courses';
-import AboutUs from './components/pages/AboutUs/AboutUs';
-import NotFound from './components/pages/NotFound/NotFound';
+import { useRoutes } from 'react-router-dom';
+import listRoute from './routes';
 
 const App = () => {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<MainRoute />}>
-          <Route path='/Home' element={<Home />} />
-          <Route path='/Article/:idArticle' element={<Article />} />
-          <Route path='/Article/:editCreate/:idArticle' element={<ArticleEditCreate />} />
-          <Route path='/Articles' element={<Articles />} />
-          <Route path='/Course/:idCourse' element={<Course />} />
-          <Route path='/Courses' element={<Courses />} />
-          <Route path='/About-us' element={<AboutUs />} />
-          <Route path='*' element={<NotFound />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  );
+  const routes = useRoutes(listRoute);
+  return routes;
 };
 export default App;
